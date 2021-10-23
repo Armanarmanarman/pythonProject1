52f730ce21b3efe47605288f26388cf3bb62e0eb
Arman <armansanatovich@gmail.com>
2021-10-23 16:22:59 +0600

    main.py  edited

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from pycoingecko import CoinGeckoAPI
 CoinGeckoAPI = CoinGeckoAPI()
 s = CoinGeckoAPI.get_coins_markets(vs_currency='usd')
 filterRange = int(input())
-for i in range(filterRange):
+i =0
+while i < filterRange:
+    print(s[i]['market_cap_rank'])
+    print(s[i]['name'])
     print(s[i]['market_cap'])
+    i = i+1
